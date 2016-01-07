6ded1978ea60133519e1bdbecec4e5deb22e682f
LewDev <lewis.a.w@hotmail.com>
2016-01-07 22:24:54 +0000

    Array Tutorial

---
+++ Main.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Sony VAIO on 08/12/2015.
+ */
+public class Main {
+
+    public static void main(String[] args){
+
+        ArraysTest arr = new ArraysTest();
+        //arr.numberArray(new int[5]);
+        //arr.oneDbox(new int[10]);
+        arr.twoDbox(new String[10][10]);
+       // arr.ThreeDArray(new String[2][2][2]);
+        //arr.enchanedArray(new int[5]);
+        //System.out.println();
+        //arr.copyArray();
+    }
+}
